c3bc67870ea111bb785ea49da23afe9859c1ed07
Behruz1227 <behruzxonniyozov12@gmail.com>
2023-10-24 15:04:47 +0500

    dytfghvjfyu

--- src/components/footer/FooterAuth.js
+++ src/components/footer/FooterAuth.js
@@ -36,11 +36,11 @@ export default function Footer() {
         {" "}
         &copy; {1900 + new Date().getYear()}
         <Text as='span' fontWeight='500' ms='4px'>
-          Horizon UI. All Rights Reserved. Made with love by
+          IT CITY ACADEMY. All Rights Reserved. Made with 
           <Link
             mx='3px'
             color={textColor}
-            href='https://www.simmmple.com?ref=horizon-chakra-free'
+            href=''
             target='_blank'
             fontWeight='700'>
             Simmmple!
