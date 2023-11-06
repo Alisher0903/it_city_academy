72c260a754a47985904e21f5579f51af7a08fe39
Ozodbek <ozodbek007@gmail.com>
2023-11-07 00:09:53 +0500

    user page accordion

--- src/student/views/admin/marketplace/index.jsx
+++ src/student/views/admin/marketplace/index.jsx
@@ -1,132 +1,105 @@
 import React from "react";
-import {
-  Box,
-  Button,
-  Flex,
-  Grid,
-  Link,
-  Text,
-  useColorModeValue,
-  SimpleGrid,
-} from "@chakra-ui/react";
+import {Box, Flex, Grid, Link, SimpleGrid, Text, useColorModeValue,} from "@chakra-ui/react";
 
 // Custom components
-import Banner from "../../../views/admin/marketplace/components/Banner";
-import TableTopCreators from "../../../views/admin/marketplace/components/TableTopCreators";
-import HistoryItem from "../../../views/admin/marketplace/components/HistoryItem";
-import NFT from "../../../components/card/NFT";
-import Card from "../../../components/card/Card.js";
-
 // Assets
-import Nft1 from "../../../assets/img/nfts/Nft1.png";
-import Nft2 from "../../../assets/img/nfts/Nft2.png";
-import Nft3 from "../../../assets/img/nfts/Nft3.png";
-import Nft4 from "../../../assets/img/nfts/Nft4.png";
-import Nft5 from "../../../assets/img/nfts/Nft5.png";
-import Nft6 from "../../../assets/img/nfts/Nft6.png";
-import Avatar1 from "../../../assets/img/avatars/avatar1.png";
-import Avatar2 from "../../../assets/img/avatars/avatar2.png";
-import Avatar3 from "../../../assets/img/avatars/avatar3.png";
-import Avatar4 from "../../../assets/img/avatars/avatar4.png";
-import tableDataTopCreators from "../../../views/admin/marketplace/variables/tableDataTopCreators.json";
-import { tableColumnsTopCreators } from "../../../views/admin/marketplace/variables/tableColumnsTopCreators";
+import {AccordionBody, AccordionHeader, AccordionItem, UncontrolledAccordion} from "reactstrap";
 
 export default function Marketplace() {
-  // Chakra Color Mode
-  const textColor = useColorModeValue("secondaryGray.900", "white");
-  const textColorBrand = useColorModeValue("brand.500", "white");
-  return (
-    <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
-      {/* Main Fields */}
-      <Grid
-        mb='20px'
-        gridTemplateColumns={{ xl: "repeat(3, 1fr)", "2xl": "1fr 0.46fr" }}
-        gap={{ base: "20px", xl: "20px" }}
-        display={{ base: "block", xl: "grid" }}>
-        <Flex
-          flexDirection='column'
-          gridArea={{ xl: "1 / 1 / 2 / 4", "2xl": "1 / 1 / 2 / 3" }}>
-          {/* <Banner /> */}
-          <Flex direction='column'>
-            <Flex
-              mt='45px'
-              mb='20px'
-              justifyContent='space-between'
-              direction={{ base: "column", md: "row" }}
-              align={{ base: "start", md: "center" }}>
-              <Text color={textColor} fontSize='2xl' ms='24px' fontWeight='700'>
-                Category
-              </Text>
-              <Flex
-                align='center'
-                me='20px'
-                ms={{ base: "24px", md: "0px" }}
-                mt={{ base: "20px", md: "0px" }}>
-                <Link
-                  color={textColorBrand}
-                  fontWeight='500'
-                  me={{ base: "34px", md: "44px" }}
-                  to='#art'>
-                  All
-                </Link>
-                <Link
-                  color={textColorBrand}
-                  fontWeight='500'
-                  me={{ base: "34px", md: "44px" }}
-                  to='#music'>
-                  Front End
-                </Link>
-                <Link
-                  color={textColorBrand}
-                  fontWeight='500'
-                  me={{ base: "34px", md: "44px" }}
-                  to='#collectibles'>
-                  Back End
-                </Link>
-                <Link color={textColorBrand} fontWeight='500' to='#sports'>
-                  3D Max
-                </Link>
-              </Flex>
-            </Flex>
-            <SimpleGrid columns={{ base: 1, md: 3, xl: 4 }} gap='20px'>
+    // Chakra Color Mode
+    const textColor = useColorModeValue("secondaryGray.900", "white");
+    const textColorBrand = useColorModeValue("brand.500", "white");
+    return (
+        <Box pt={{base: "180px", md: "80px", xl: "80px"}}>
+            {/* Main Fields */}
+            <Grid
+                mb='20px'
+                gridTemplateColumns={{xl: "repeat(3, 1fr)", "2xl": "1fr 0.46fr"}}
+                gap={{base: "20px", xl: "20px"}}
+                display={{base: "block", xl: "grid"}}>
+                <Flex
+                    flexDirection='column'
+                    gridArea={{xl: "1 / 1 / 2 / 4", "2xl": "1 / 1 / 2 / 3"}}>
+                    {/* <Banner /> */}
+                    <Flex direction='column'>
+                        <Flex
+                            mt='45px'
+                            mb='20px'
+                            justifyContent='space-between'
+                            direction={{base: "column", md: "row"}}
+                            align={{base: "start", md: "center"}}>
+                            <Text color={textColor} fontSize='2xl' ms='24px' fontWeight='700'>
+                                Category
+                            </Text>
+                            <Flex
+                                align='center'
+                                me='20px'
+                                ms={{base: "24px", md: "0px"}}
+                                mt={{base: "20px", md: "0px"}}>
+                                <Link
+                                    color={textColorBrand}
+                                    fontWeight='500'
+                                    me={{base: "34px", md: "44px"}}
+                                    to='#art'>
+                                    All
+                                </Link>
+                                <Link
+                                    color={textColorBrand}
+                                    fontWeight='500'
+                                    me={{base: "34px", md: "44px"}}
+                                    to='#music'>
+                                    Front End
+                                </Link>
+                                <Link
+                                    color={textColorBrand}
+                                    fontWeight='500'
+                                    me={{base: "34px", md: "44px"}}
+                                    to='#collectibles'>
+                                    Back End
+                                </Link>
+                                <Link color={textColorBrand} fontWeight='500' to='#sports'>
+                                    3D Max
+                                </Link>
+                            </Flex>
+                        </Flex>
 
-              {/* cards */}
-              <NFT
-                name='Abstract Colors'
-                // author='By Esthera Jackson'
-                bidders={[
-                  // Avatar1,
-                  // Avatar2,
-                  // Avatar3,
-                  // Avatar4,
-                  // Avatar1,
-                  // Avatar1,
-                  // Avatar1,
-                  // Avatar1,
-                ]}
-                image={Nft1}
-                // currentbid='0.91 ETH'
-                download='frontend'
-              />
-              <NFT
-                name='Abstract Colors'
-                // author='By Esthera Jackson'
-                bidders={[
-                  // Avatar1,
-                  // Avatar2,
-                  // Avatar3,
-                  // Avatar4,
-                  // Avatar1,
-                  // Avatar1,
-                  // Avatar1,
-                  // Avatar1,
-                ]}
-                image={Nft1}
-                // currentbid='0.91 ETH'
-                download='frontend'
-              />
-            </SimpleGrid>
-            {/* <Text
+                        {/*    Accardion */}
+                        <section className="d-flex w-100 justify-content-center mt-3">
+                            <UncontrolledAccordion className="w-75">
+                                <AccordionItem>
+                                    <AccordionHeader targetId="1">
+                                        Accordion Item 1
+                                    </AccordionHeader>
+                                    <AccordionBody accordionId="1">
+                                        <strong>
+                                            This is the first item's accordion body.
+                                        </strong>
+                                    </AccordionBody>
+                                </AccordionItem>
+                                <AccordionItem>
+                                    <AccordionHeader targetId="2">
+                                        Accordion Item 2
+                                    </AccordionHeader>
+                                    <AccordionBody accordionId="2">
+                                        <strong>
+                                            This is the second item's accordion body.
+                                        </strong>
+                                    </AccordionBody>
+                                </AccordionItem>
+                                <AccordionItem>
+                                    <AccordionHeader targetId="3">
+                                        Accordion Item 3
+                                    </AccordionHeader>
+                                    <AccordionBody accordionId="3">
+                                        <strong>
+                                            This is the third item's accordion body.
+                                        </strong>
+                                    </AccordionBody>
+                                </AccordionItem>
+                            </UncontrolledAccordion>
+                        </section>
+
+                        {/* <Text
               mt='45px'
               mb='36px'
               color={textColor}
@@ -135,9 +108,9 @@ export default function Marketplace() {
               fontWeight='700'>
               
             </Text> */}
-          </Flex>
-        </Flex>
-        {/* <Flex
+                    </Flex>
+                </Flex>
+                {/* <Flex
           flexDirection='column'
           gridArea={{ xl: "1 / 3 / 2 / 4", "2xl": "1 / 2 / 2 / 3" }}>
           <Card px='0px' mb='20px'>
@@ -203,8 +176,8 @@ export default function Marketplace() {
             />
           </Card>
         </Flex> */}
-      </Grid>
-      {/* Delete Product */}
-    </Box>
-  );
+            </Grid>
+            {/* Delete Product */}
+        </Box>
+    );
 }
