3d49189c89793679c1f296a489edea4cb25bf706
Ozodbek <ozodbek007@gmail.com>
2023-11-06 19:18:40 +0500

    group teacher firstName add

--- src/api/routers.js
+++ src/api/routers.js
@@ -7,8 +7,27 @@ export const getCategory = (setCategory) => {
 }
 
 export function getGroup(setGroup) {
-    axios.get(api + "group?page=0&size=10", config)
-        .then(res => {
-            setGroup(res.data.body.object)
-        })
+    axios.get(api + "user/teacher", config)
+        .then(t => {
+            let group = [];
+            axios.get(api + "group?page=0&size=10", config)
+                .then(res => {
+                    for (const g of res.data.body.object)
+                        group.push({
+                            id: g.id,
+                            name: g.name,
+                            teacher: teacherById(g.teacherId)
+                        });
+                    setGroup(group);
+                });
+
+            function teacherById(id) {
+                for (const teacher of t.data.body) if (teacher.id === id) return teacher.firstName;
+            }
+        });
+}
+
+export const getTeacher = (setTeacher) => {
+    axios.get(api + "user/teacher", config)
+        .then(res => setTeacher(res.data.body));
 }
