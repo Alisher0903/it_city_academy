339dbd20fee3625e5b8961d1d1e720db47c13cce
Behruz1227 <behruzxonniyozov12@gmail.com>
2023-11-09 17:38:06 +0500

    jrehufjkregb

--- src/api/api.js
+++ src/api/api.js
@@ -11,7 +11,7 @@ export const getGeftsTeacher = "gift";
 export const messageAdd = "message/save";
 
 // gidts url
-export const giftAdd = "gift/save/";
+export const giftAdd = "gift/save";
 export const giftDelete = "gift/delete/";
 export const giftEdit = "gift/update/";
 
@@ -19,14 +19,14 @@ export const giftEdit = "gift/update/";
 export const imgUrl = api + "attachment/getFile/";
 
 // category url
-export const categoryAdd = "category/save/";
+export const categoryAdd = "category/save";
 export const categoryEdit = "category/update/";
 export const categoryDelete = "category/active/";
 
 // group url
 export const groupAdd = "group/save";
-export const groupEdit = "group/update";
-export const groupDelete = "group/isactive";
+export const groupEdit = "group/update/";
+export const groupDelete = "group/isactive/";
 
 // autinficatsiya jwt tokin
 export const config = {
