7fb618e344ac0036471de6e4c2f898a625bcdbd6
Behruz1227 <behruzxonniyozov12@gmail.com>
2023-11-13 16:08:47 +0500

    tgv

--- src/student/views/admin/marketplace/components/TestList.js
+++ src/student/views/admin/marketplace/components/TestList.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {AccordionBody, Button, Col, Row} from "reactstrap";
-import {getCategoryByTest} from "../../../../../api/routers";
+import React, { useEffect, useState } from 'react';
+import { AccordionBody, Button, Col, Row } from "reactstrap";
+import { getCategoryByTest } from "../../../../../api/routers";
 import '../../../../../assets/css/test.css'
+import { Link } from '@chakra-ui/react';
 
-function TestList({categoryId, index}) {
+function TestList({ categoryId, index }) {
 
     const [test, setTest] = useState([]);
 
@@ -15,21 +16,29 @@ function TestList({categoryId, index}) {
 
     return (
         <AccordionBody accordionId={index} className="testb">
+            <Link to="/Student/test/coding"></Link>
             {test.length ? test.map((item, i) =>
-                    <Row className="py-4 border test-row w-100 ms-0 my-2" key={i}>
-                        <Col className="col-1 d-flex align-items-center justify-content-center">
-                            <img src="https://blob.sololearn.com/web-assets/le-lesson-icon.svg" alt="coin lesson"/>
-                        </Col>
-                        <Col className='col-11'>
-                            <p className='' style={{fontSize: ".8em", color: '#5b5b5b'}}>Lesson</p>
-                            <p className='fs-5 fw-bold text-dark'>{item.question}</p>
-                            <p style={{fontSize: '.85em', borderRadius: "18px", top: ".4em"}} className='d-inline border py-1 px-2 position-relative'><span className='fw-bold'>XP</span> +{item.grade}</p>
-                        </Col>
-                        <div className='w-100 d-flex justify-content-center mt-3'>
-                            <Button style={{width: "99%", backgroundColor: '#2493DF'}} className='border-0 py-2'>Learn</Button>
-                        </div>
-                    </Row>
-                ) :
+                <Row className="py-4 border test-row w-100 ms-0 my-2" key={i}>
+                    <Col className="col-1 d-flex align-items-center justify-content-center">
+                        <img src="https://blob.sololearn.com/web-assets/le-lesson-icon.svg" alt="coin lesson" />
+                    </Col>
+                    <Col className='col-11'>
+                        <p className='' style={{ fontSize: ".8em", color: '#5b5b5b' }}>Lesson</p>
+                        <p className='fs-5 fw-bold text-dark'>{item.question}</p>
+                        <p style={{ fontSize: '.85em', borderRadius: "18px", top: ".4em" }} className='d-inline border py-1 px-2 position-relative'><span className='fw-bold'>XP</span> +{item.grade}</p>
+                    </Col>
+                    <div className='w-100 d-flex justify-content-center mt-3'>
+                        <Button
+                        onClick={() => {
+                            // sessionStorage.setItem("learnId", item);
+                            // console.log(item);
+
+                        }}
+                            style={{ width: "99%", backgroundColor: '#2493DF' }}
+                            className='border-0 py-2'>Learn</Button>
+                    </div>
+                </Row>
+            ) :
                 <p>Test not found</p>}
         </AccordionBody>
 
