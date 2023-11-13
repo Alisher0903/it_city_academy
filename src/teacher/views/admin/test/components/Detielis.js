581c1ba313947a72611ee1552154faedc75c7388
alishersodiqov09 <alishersodiqov09@gmail.com>
2023-11-14 00:23:29 +0500

    1

--- src/teacher/views/admin/test/components/Detielis.js
+++ src/teacher/views/admin/test/components/Detielis.js
@@ -1,8 +1,14 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
+
 function Detielis() {
+    const textColor = useColorModeValue("navy.700", "white");
+
     return (
-        <div>
+        <Box
+            color={textColor}
+            pt={{ base: "130px", md: "80px", xl: "100px" }}>
             Detielis.js
-        </div>
+        </Box>
     );
 }
 
