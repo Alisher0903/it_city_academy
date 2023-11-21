fe72f7d993267a4415ceb11b9876e4c131bfe259
alishersodiqov09 <alishersodiqov09@gmail.com>
2023-11-21 16:19:50 +0500

    sahifaga kirganda reload buldi

--- src/teacher/views/admin/default/index.jsx
+++ src/teacher/views/admin/default/index.jsx
@@ -1,12 +1,3 @@
-/*!
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
-// Chakra imports
 import {
   Avatar,
   Box,
@@ -17,26 +8,24 @@ import {
   SimpleGrid,
   useColorModeValue,
 } from "@chakra-ui/react";
-// Assets
 import Usa from "../../../assets/img/dashboards/usa.png";
-// Custom components
 import MiniCalendar from "../../../../components/calendar/MiniCalendar";
 import MiniStatistics from "../../../../components/card/MiniStatistics";
 import IconBox from "../../../components/icons/IconBox";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   MdAddTask,
   MdAttachMoney,
   MdBarChart,
   MdFileCopy,
 } from "react-icons/md";
-import CheckTable from "../../../views/admin/default/components/CheckTable";
-import ComplexTable from "../../../views/admin/default/components/ComplexTable";
-import DailyTraffic from "../../../views/admin/default/components/DailyTraffic";
-import PieCard from "../../../views/admin/default/components/PieCard";
-import Tasks from "../../../views/admin/default/components/Tasks";
-import TotalSpent from "../../../views/admin/default/components/TotalSpent";
-import WeeklyRevenue from "../../../views/admin/default/components/WeeklyRevenue";
+import CheckTable from "./components/CheckTable";
+import ComplexTable from "./components/ComplexTable";
+import DailyTraffic from "./components/DailyTraffic";
+import PieCard from "./components/PieCard";
+import Tasks from "./components/Tasks";
+import TotalSpent from "./components/TotalSpent";
+import WeeklyRevenue from "./components/WeeklyRevenue";
 import {
   columnsDataCheck,
   columnsDataComplex,
@@ -45,9 +34,16 @@ import tableDataCheck from "../../../views/admin/default/variables/tableDataChec
 import tableDataComplex from "../../../views/admin/default/variables/tableDataComplex.json";
 
 export default function UserReports() {
-  // Chakra Color Mode
   const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+
+  useEffect(() => {
+    if (sessionStorage.getItem('reload') !== "true") {
+      sessionStorage.setItem('reload', 'true')
+      window.location.reload();
+    }
+  }, []);
+
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
