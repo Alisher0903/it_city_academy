5dd504c2a40d9f7908f2a2f235df03eba3cc9833
alishersodiqov09 <alishersodiqov09@gmail.com>
2023-11-22 20:08:14 +0500

    teacher dashboard style full backend ulash qoldi

--- src/teacher/views/admin/default/components/WeeklyRevenue.js
+++ src/teacher/views/admin/default/components/WeeklyRevenue.js
@@ -1,70 +1,57 @@
-// Chakra imports
 import {
-  Box,
-  Button,
-  Flex,
-  Icon,
-  Text,
-  useColorModeValue,
+    Select,
+    Table,
+    TableCaption,
+    TableContainer,
+    Tbody,
+    Td,
+    Text,
+    Th,
+    Thead,
+    Tr,
 } from "@chakra-ui/react";
 import Card from "../../../../components/card/Card.js";
-// Custom components
-import BarChart from "../../../../components/charts/BarChart";
 import React from "react";
-import {
-  barChartDataConsumption,
-  barChartOptionsConsumption,
-} from "../../../../variables/charts";
-import { MdBarChart } from "react-icons/md";
 
 export default function WeeklyRevenue(props) {
-  const { ...rest } = props;
+    const {...rest} = props;
 
-  // Chakra Color Mode
-  const textColor = useColorModeValue("secondaryGray.900", "white");
-  const iconColor = useColorModeValue("brand.500", "white");
-  const bgButton = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
-  const bgHover = useColorModeValue(
-    { bg: "secondaryGray.400" },
-    { bg: "whiteAlpha.50" }
-  );
-  const bgFocus = useColorModeValue(
-    { bg: "secondaryGray.300" },
-    { bg: "whiteAlpha.100" }
-  );
-  return (
-    <Card align='center' direction='column' w='100%' {...rest}>
-      <Flex align='center' w='100%' px='15px' py='10px'>
-        <Text
-          me='auto'
-          color={textColor}
-          fontSize='xl'
-          fontWeight='700'
-          lineHeight='100%'>
-          Weekly Revenue
-        </Text>
-        <Button
-          align='center'
-          justifyContent='center'
-          bg={bgButton}
-          _hover={bgHover}
-          _focus={bgFocus}
-          _active={bgFocus}
-          w='37px'
-          h='37px'
-          lineHeight='100%'
-          borderRadius='10px'
-          {...rest}>
-          <Icon as={MdBarChart} color={iconColor} w='24px' h='24px' />
-        </Button>
-      </Flex>
-
-      <Box h='240px' mt='auto'>
-        <BarChart
-          chartData={barChartDataConsumption}
-          chartOptions={barChartOptionsConsumption}
-        />
-      </Box>
-    </Card>
-  );
+    return (
+        <Card {...rest}>
+            <Text
+                display="flex"
+                justifyContent="space-between">
+                <span className="ms-1 mt-1 fs-5 fw-semibold">Name guruh bo'yicha top 5</span>
+                <Select placeholder="select" w="25%">
+                    <option value='option1'>group 1</option>
+                </Select>
+            </Text>
+            <TableContainer
+                mt="1rem"
+                pt=".7rem"
+                pb=".7rem"
+                borderRadius="15px"
+                boxShadow="rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px"
+            >
+                <Table>
+                    <TableCaption
+                        fontSize="1rem">name Group</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>T/r</Th>
+                            <Th>full name</Th>
+                            <Th>phone number</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>1</Td>
+                            <Td>sodiqov a</Td>
+                            <Td>123456</Td>
+                        </Tr>
+                    </Tbody>
+                </Table>
+            </TableContainer>
+        </Card>
+    );
 }
--- src/teacher/views/admin/default/index.jsx
+++ src/teacher/views/admin/default/index.jsx
@@ -1,154 +1,170 @@
 import {
-  Avatar,
-  Box,
-  Flex,
-  FormLabel,
-  Icon,
-  Select,
-  SimpleGrid,
-  useColorModeValue,
+    Avatar,
+    Box,
+    Flex,
+    FormLabel,
+    Icon,
+    Select,
+    SimpleGrid,
+    useColorModeValue,
 } from "@chakra-ui/react";
 import Usa from "../../../assets/img/dashboards/usa.png";
 import MiniCalendar from "../../../../components/calendar/MiniCalendar";
 import MiniStatistics from "../../../../components/card/MiniStatistics";
 import IconBox from "../../../components/icons/IconBox";
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import {
-  MdAddTask,
-  MdAttachMoney,
-  MdBarChart,
-  MdFileCopy,
+    MdAddTask,
+    MdAttachMoney,
+    MdBarChart, MdCurrencyExchange,
+    MdFileCopy, MdPeople, MdPerson, MdStackedBarChart,
 } from "react-icons/md";
+import TotalSpent from "./components/TotalSpent";
+import WeeklyRevenue from "./components/WeeklyRevenue";
+
 import CheckTable from "./components/CheckTable";
 import ComplexTable from "./components/ComplexTable";
 import DailyTraffic from "./components/DailyTraffic";
 import PieCard from "./components/PieCard";
 import Tasks from "./components/Tasks";
-import TotalSpent from "./components/TotalSpent";
-import WeeklyRevenue from "./components/WeeklyRevenue";
 import {
-  columnsDataCheck,
-  columnsDataComplex,
-} from "../../../views/admin/default/variables/columnsData";
-import tableDataCheck from "../../../views/admin/default/variables/tableDataCheck.json";
-import tableDataComplex from "../../../views/admin/default/variables/tableDataComplex.json";
+    columnsDataCheck,
+    columnsDataComplex,
+} from "./variables/columnsData";
+import tableDataCheck from "./variables/tableDataCheck.json";
+import tableDataComplex from "./variables/tableDataComplex.json";
 
 export default function UserReports() {
-  const brandColor = useColorModeValue("brand.500", "white");
-  const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+    const brandColor = useColorModeValue("brand.500", "white");
+    const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
 
-  useEffect(() => {
-    if (sessionStorage.getItem('reloadTeacher') !== "true") {
-      sessionStorage.setItem('reloadTeacher', 'true')
-      window.location.reload();
-    }
-  }, []);
+    useEffect(() => {
+        if (sessionStorage.getItem('reloadTeacher') !== "true") {
+            sessionStorage.setItem('reloadTeacher', 'true')
+            window.location.reload();
+        }
+    }, []);
 
-  return (
-    <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-      <SimpleGrid
-        columns={{ base: 1, md: 2, lg: 3, "2xl": 6 }}
-        gap='20px'
-        mb='20px'>
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdBarChart} color={brandColor} />
-              }
-            />
-          }
-          name='Earnings'
-          value='$350.4'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor} />
-              }
-            />
-          }
-          name='Spend this month'
-          value='$642.39'
-        />
-        <MiniStatistics growth='+23%' name='Sales' value='$574.34' />
-        <MiniStatistics
-          endContent={
-            <Flex me='-16px' mt='10px'>
-              <FormLabel htmlFor='balance'>
-                <Avatar src={Usa} />
-              </FormLabel>
-              <Select
-                id='balance'
-                variant='mini'
-                mt='5px'
-                me='0px'
-                defaultValue='usd'>
-                <option value='usd'>USD</option>
-                <option value='eur'>EUR</option>
-                <option value='gba'>GBA</option>
-              </Select>
-            </Flex>
-          }
-          name='Your balance'
-          value='$1,000'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'
-              icon={<Icon w='28px' h='28px' as={MdAddTask} color='white' />}
-            />
-          }
-          name='New Tasks'
-          value='154'
-        />
-        <MiniStatistics
-          startContent={
-            <IconBox
-              w='56px'
-              h='56px'
-              bg={boxBg}
-              icon={
-                <Icon w='32px' h='32px' as={MdFileCopy} color={brandColor} />
-              }
-            />
-          }
-          name='Total Projects'
-          value='2935'
-        />
-      </SimpleGrid>
+    return (
+        <Box pt={{base: "130px", md: "80px", xl: "80px"}}>
+            <SimpleGrid
+                columns={{base: 1, md: 2, lg: 2, "2xl": 4}}
+                gap='20px'
+                mb='20px'>
+                <MiniStatistics
+                    startContent={
+                        <IconBox
+                            w='56px'
+                            h='56px'
+                            bg={boxBg}
+                            icon={
+                                <Icon w='32px' h='32px' as={MdPeople} color={brandColor}/>
+                            }
+                        />
+                    }
+                    name="Guruhlarning umumiy soni"
+                    value='350.4'
+                />
+                <MiniStatistics
+                    startContent={
+                        <IconBox
+                            w='56px'
+                            h='56px'
+                            bg={boxBg}
+                            icon={
+                                <Icon w='32px' h='32px' as={MdPerson} color={brandColor}/>
+                            }
+                        />
+                    }
+                    name="O'quvchilarning umumiy soni"
+                    value='642.39'
+                />
+                <MiniStatistics
+                    startContent={
+                        <IconBox
+                            w='56px'
+                            h='56px'
+                            bg={boxBg}
+                            icon={
+                                <Icon w='32px' h='32px' as={MdCurrencyExchange} color={brandColor}/>
+                            }
+                        />
+                    }
+                    name="O'quvchilarning umumiy coinlari soni"
+                    value='574.34'/>
+                <MiniStatistics
+                    startContent={
+                        <IconBox
+                            w='56px'
+                            h='56px'
+                            bg={boxBg}
+                            icon={
+                                <Icon w='32px' h='32px' as={MdAttachMoney} color={brandColor}/>
+                            }
+                        />
+                    }
+                    name="Sovg'a olganlar soni"
+                    value='2935'
+                />
 
-      <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px' mb='20px'>
-        <TotalSpent />
-        <WeeklyRevenue />
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck} />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <DailyTraffic />
-          <PieCard />
-        </SimpleGrid>
-      </SimpleGrid>
-      <SimpleGrid columns={{ base: 1, md: 1, xl: 2 }} gap='20px' mb='20px'>
-        <ComplexTable
-          columnsData={columnsDataComplex}
-          tableData={tableDataComplex}
-        />
-        <SimpleGrid columns={{ base: 1, md: 2, xl: 2 }} gap='20px'>
-          <Tasks />
-          <MiniCalendar h='100%' minW='100%' selectRange={false} />
-        </SimpleGrid>
-      </SimpleGrid>
-    </Box>
-  );
+                {/*<MiniStatistics*/}
+                {/*    endContent={*/}
+                {/*        <Flex me='-16px' mt='10px'>*/}
+                {/*            <FormLabel htmlFor='balance'>*/}
+                {/*                <Avatar src={Usa}/>*/}
+                {/*            </FormLabel>*/}
+                {/*            <Select*/}
+                {/*                id='balance'*/}
+                {/*                variant='mini'*/}
+                {/*                mt='5px'*/}
+                {/*                me='0px'*/}
+                {/*                defaultValue='usd'>*/}
+                {/*                <option value='usd'>USD</option>*/}
+                {/*                <option value='eur'>EUR</option>*/}
+                {/*                <option value='gba'>GBA</option>*/}
+                {/*            </Select>*/}
+                {/*        </Flex>*/}
+                {/*    }*/}
+                {/*    name='Your balance'*/}
+                {/*    value='$1,000'*/}
+                {/*/>*/}
+                {/*<MiniStatistics*/}
+                {/*    startContent={*/}
+                {/*        <IconBox*/}
+                {/*            w='56px'*/}
+                {/*            h='56px'*/}
+                {/*            bg='linear-gradient(90deg, #4481EB 0%, #04BEFE 100%)'*/}
+                {/*            icon={<Icon w='28px' h='28px' as={MdAddTask} color='white'/>}*/}
+                {/*        />*/}
+                {/*    }*/}
+                {/*    name='New Tasks'*/}
+                {/*    value='154'*/}
+                {/*/>*/}
+            </SimpleGrid>
+
+            <SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px'>
+                <TotalSpent/>
+                <WeeklyRevenue/>
+            </SimpleGrid>
+
+            {/*comments dashboard*/}
+            {/*<SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>*/}
+            {/*    <CheckTable columnsData={columnsDataCheck} tableData={tableDataCheck}/>*/}
+            {/*    <SimpleGrid columns={{base: 1, md: 2, xl: 2}} gap='20px'>*/}
+            {/*        <DailyTraffic/>*/}
+            {/*        <PieCard/>*/}
+            {/*    </SimpleGrid>*/}
+            {/*</SimpleGrid>*/}
+            {/*<SimpleGrid columns={{base: 1, md: 1, xl: 2}} gap='20px' mb='20px'>*/}
+            {/*    <ComplexTable*/}
+            {/*        columnsData={columnsDataComplex}*/}
+            {/*        tableData={tableDataComplex}*/}
+            {/*    />*/}
+            {/*    <SimpleGrid gap='20px'>*/}
+            {/*        <Tasks/>*/}
+            {/*        <MiniCalendar h='100%' w='100%' selectRange={false}/>*/}
+            {/*    </SimpleGrid>*/}
+            {/*</SimpleGrid>*/}
+        </Box>
+    );
 }
