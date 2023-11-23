4ad90b3791e31e267d50c0a211026b9e48e61dc5
alishersodiqov09 <alishersodiqov09@gmail.com>
2023-11-23 19:07:52 +0500

    teacher dashboard 95% buldi

--- src/teacher/views/admin/exchange/index.jsx
+++ src/teacher/views/admin/exchange/index.jsx
@@ -8,10 +8,10 @@ import {
 } from "@chakra-ui/react";
 import Card from "../../../components/card/Card";
 import categoryImg from "../marketplace/categoryImg.png";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import { api } from "api/api";
-import { config } from "api/api";
+import {api, config} from "../../../../api/api";
+import {toast, ToastContainer} from "react-toastify";
 
 function Exchange() {
     const textColor1 = useColorModeValue("navy.700", "white");
@@ -23,14 +23,19 @@ function Exchange() {
     }, []);
 
     // getExchange
-    const getExchange = () => axios.get(api + "exchange/teacher", config)
-        .then(res => setExchanges(res.data));
-
-
-    console.log(exchanges);
+    const getExchange = () => {
+        axios.get(api + "exchange/teacher?page=0&size=100", config)
+            .then(res => setExchanges(res.data.body))
+            .catch(err => {
+                toast.error("Exchange not found");
+                console.log(err)
+            })
+    }
+    console.log(exchanges)
 
     return (
         <>
+            <ToastContainer/>
             <Text
                 pt="100px"
                 ms="2px"
@@ -39,12 +44,12 @@ function Exchange() {
                 fontWeight='bold'>
                 Exchange
             </Text>
-            <SimpleGrid columns={{ base: 1, md: 3, xl: 4 }} gap='20px' mt="15px">
+            <SimpleGrid columns={{base: 1, md: 3, xl: 4}} gap='20px' mt="15px">
                 <Card
                     boxShadow="rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;"
                     p='20px'>
-                    <Flex direction={{ base: "column" }} justify='center'>
-                        <Box mb={{ base: "20px", "2xl": "20px" }} position='relative'>
+                    <Flex direction={{base: "column"}} justify='center'>
+                        <Box position='relative'>
                             <Image
                                 objectFit="cover"
                                 src={categoryImg}
@@ -56,18 +61,28 @@ function Exchange() {
                         </Box>
                         <Text
                             color={textColor1}
-                            fontSize={{
-                                base: "xl",
-                                md: "lg",
-                                lg: "lg",
-                                xl: "lg",
-                                "2xl": "md",
-                                "3xl": "lg",
-                            }}
-                            mb='5px'
+                            fontSize="1.5rem"
                             fontWeight='bold'
+                            ms='4px'
+                            mt="10px">
+                            Exchange name
+                        </Text>
+                        <Text
+                            color={textColor1}
+                            fontSize="1rem"
+                            mb='5px'
+                            fontWeight="normal"
                             ms='4px'>
-                            name
+                            Exchange ball
+                        </Text>
+                        <Text
+                            color={textColor1}
+                            fontSize="1rem"
+                            mb='5px'
+                            fontWeight="bolder"
+                            ms='4px'
+                            mt="8px">
+                            Full Name
                         </Text>
                     </Flex>
                 </Card>
