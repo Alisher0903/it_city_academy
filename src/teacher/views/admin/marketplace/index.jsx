9fbedcce218aae7d4287a5def30c16b3ac116ca4
Ozodbek <ozodbek007@gmail.com>
2023-11-24 10:50:41 +0500

    user page test style editing

--- src/teacher/views/admin/marketplace/index.jsx
+++ src/teacher/views/admin/marketplace/index.jsx
@@ -1,22 +1,11 @@
 import React, {useEffect, useState} from "react";
-import {
-    Box,
-    Flex,
-    Icon,
-    Image,
-    Link,
-    Text,
-    SimpleGrid,
-    Grid,
-    useColorModeValue
-} from "@chakra-ui/react";
+import {Box, Flex, Icon, Image, Link, SimpleGrid, Text, useColorModeValue} from "@chakra-ui/react";
 import Card from "../../../components/card/Card";
 import {MdDelete, MdEdit} from "react-icons/md";
 import {Button, Modal, ModalBody, ModalFooter, ModalHeader} from "reactstrap";
 import categoryImg from "./categoryImg.png";
 import axios from "axios";
 import {api, config, imgUrl} from "../../../../api/api";
-import question from "../test/question.png";
 
 export default function Marketplace() {
     const textColor = useColorModeValue("secondaryGray.900", "white");
@@ -53,7 +42,7 @@ export default function Marketplace() {
                 <Text color={textColor} fontSize='2xl' ms='10px' fontWeight='700'>
                     Category
                 </Text>
-                {/*<Button className="rounded-5 addBtn" color="primary" onClick={openAddModal}>Add Category</Button>*/}
+                <Button className="rounded-5 addBtn" color="primary" onClick={openAddModal}>Add Category</Button>
                 {/*addModal*/}
                 <Modal isOpen={addModal} centered size="lg">
                     <ModalHeader className="techer__modal-head" toggle={openAddModal}>Add Teacher Category</ModalHeader>
@@ -124,27 +113,27 @@ export default function Marketplace() {
                                     }}
                                     mt='25px'>
                                     <Box ms="auto">
-                                        {/*<Link*/}
-                                        {/*    onClick={() => {*/}
-                                        {/*        openEditModal();*/}
-                                        {/*        // setCategoryId(categoryIdIn);*/}
-                                        {/*    }}*/}
-                                        {/*    variant='no-hover'*/}
-                                        {/*    ms='0px'*/}
-                                        {/*    me="20px"*/}
-                                        {/*    p='0px !important'>*/}
-                                        {/*    <Icon as={MdEdit} color='secondaryGray.500' h='18px' w='18px'/>*/}
-                                        {/*</Link>*/}
-                                        {/*<Link*/}
-                                        {/*    onClick={() => {*/}
-                                        {/*        openDeleteModal();*/}
-                                        {/*        // setCategoryId(categoryIdIn);*/}
-                                        {/*    }}*/}
-                                        {/*    variant='no-hover'*/}
-                                        {/*    ms='0px'*/}
-                                        {/*    p='0px !important'>*/}
-                                        {/*    <Icon as={MdDelete} color='secondaryGray.500' h='18px' w='18px'/>*/}
-                                        {/*</Link>*/}
+                                        <Link
+                                            onClick={() => {
+                                                openEditModal();
+                                                // setCategoryId(categoryIdIn);
+                                            }}
+                                            variant='no-hover'
+                                            ms='0px'
+                                            me="20px"
+                                            p='0px !important'>
+                                            <Icon as={MdEdit} color='secondaryGray.500' h='18px' w='18px'/>
+                                        </Link>
+                                        <Link
+                                            onClick={() => {
+                                                openDeleteModal();
+                                                // setCategoryId(categoryIdIn);
+                                            }}
+                                            variant='no-hover'
+                                            ms='0px'
+                                            p='0px !important'>
+                                            <Icon as={MdDelete} color='secondaryGray.500' h='18px' w='18px'/>
+                                        </Link>
                                     </Box>
                                 </Flex>
                             </Flex>
@@ -154,7 +143,8 @@ export default function Marketplace() {
 
                 {/*editModal*/}
                 <Modal isOpen={editModal} centered size="lg">
-                    <ModalHeader className="techer__modal-head" toggle={openEditModal}>Edit Teacher Category</ModalHeader>
+                    <ModalHeader className="techer__modal-head" toggle={openEditModal}>Edit Teacher
+                        Category</ModalHeader>
                     <ModalBody className="techer__modal-body">
                         editModal
                     </ModalBody>
@@ -166,7 +156,8 @@ export default function Marketplace() {
 
                 {/*deleteModal*/}
                 <Modal isOpen={deleteModal} centered>
-                    <ModalHeader className="techer__modal-head" toggle={openDeleteModal}>Delete Teacher Category</ModalHeader>
+                    <ModalHeader className="techer__modal-head" toggle={openDeleteModal}>Delete Teacher
+                        Category</ModalHeader>
                     <ModalBody className="techer__modal-delete">
                         deleteModal
                     </ModalBody>
