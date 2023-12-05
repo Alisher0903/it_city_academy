1dd2a0ae034f6caa61ee8a828e9617e506527887
alishersodiqov09 <alishersodiqov09@gmail.com>
2023-12-06 02:23:16 +0500

    admin teacher xam full buldi

--- src/views/admin/teachersAdd/index.jsx
+++ src/views/admin/teachersAdd/index.jsx
@@ -83,7 +83,7 @@ function AddTeachers() {
             email: byIdIn("email").value,
             password: byIdIn("password").value,
             phoneNumber: byIdIn("phoneNumber").value,
-            groupId: byIdIn("groupId"),
+            groupId: byIdIn("groupId").value,
             gender: byIdIn('gender').value
         }
         axios.put(api + "user/update/" + teacherGetId.id, editData, config)
@@ -94,6 +94,7 @@ function AddTeachers() {
             })
             .catch(() => {
                 toast.error("Teacherning ma'lumotlarini o'zgartirilmadi!")
+                console.log(editData)
             });
     }
 
@@ -108,7 +109,6 @@ function AddTeachers() {
             toast.error("Xatolik yuz berdi");
         });
     }
-    console.log(teacherGetId)
 
     return (
         <>
